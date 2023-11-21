84beffd7738c2ffaadac2c13e1b6aa4c669d4679
Underscore96 <ottonelloisabella@gmail.com>
2023-11-21 14:38:18 +0100

    cc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,34 @@
-import { Component, Input } from '@angular/core';
+// import { Component, Input } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.scss']
+// })
+// export class AppComponent {
+//   pizze = [
+//     { nome: 'Margherita', immagine: '....', quantita: 0, prezzo: 10 },
+//     { nome: 'Pepperoni', immagine: '...', quantita: 0,  prezzo: 13 },
+//     { nome: 'Vegetariana', immagine: '...', quantita: 0,  prezzo: 14 }
+//   ];
+//   title = 'angular-pizza';
+//   pizzaSelected: any;
+//   // pizzeNelCarrello: any[] = [];
+//   pizzeNelCarrello = [];
+//   // @Input() pizze: any;
+
+
+
+//   addPizza(pizza: any) {
+//     this.pizzaSelected = pizza;
+//   }
+
+// }
+
+
+
+// app.component.ts
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,29 +36,44 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-pizza';
+  pizze: { nome: string, immagine: string, quantita: number, prezzo: number }[] = [
+    { nome: 'Margherita', immagine: '...', quantita: 0, prezzo: 10 },
+    { nome: 'Pepperoni', immagine: '...', quantita: 0, prezzo: 13 },
+    { nome: 'Vegetariana', immagine: '...', quantita: 0, prezzo: 14 }
+  ];
+
   pizzaSelected: any;
-  pizzeNelCarrello: any[] = [];
-  @Input() pizze: any;
 
+  pizzeNelCarrello: { nome: string, immagine: string, quantita: number, prezzo: number }[] = [];
 
+  addPizza(pizza: any): void {
+    
+    const pizzaIndex = this.pizzeNelCarrello.findIndex(p => p.nome === pizza.nome);
 
-  addPizza(pizza: any) {
-    this.pizzaSelected = pizza;
-  }
-
-  aggiungiPizzaAlCarrello(pizza: any) {
-    // this.pizzeNelCarrello.push({ ...pizza, quantita: 1 });
-    if (this.pizzaSelected) {
-      const index = this.pizzeNelCarrello.findIndex(p => p.nome === this.pizzaSelected.nome);
-
-      if (index !== -1) {
-        this.pizzeNelCarrello[index].quantita += 1;
-      } else {
-        this.pizzeNelCarrello.push({ ...this.pizzaSelected, quantita: 1 });
-      }
+    if (pizzaIndex !== -1) {
+    
+      this.pizzeNelCarrello[pizzaIndex].quantita++;
+    } else {
+      
+      this.pizzeNelCarrello.push({ ...pizza, quantita: 1 });
     }
   }
 
+  // aggiungiPizzaAlCarrello(pizza: any): void {
+  //   this.addPizza(pizza);
+  // }
+
+  // rimuoviPizzaDalCarrello(carrello: any[]): void {
+  //   this.pizzeNelCarrello = carrello;
+  // }
+  aggiungiPizzaAlCarrello(pizza: any): void {
+    this.addPizza(pizza);
+  }
+
+  rimuoviPizzaDalCarrello(carrello: any[]): void {
+    this.pizzeNelCarrello = carrello;
+  }
   
 }
+
+
--- src/app/carrello/carrello.component.ts
+++ src/app/carrello/carrello.component.ts
@@ -1,3 +1,44 @@
+// import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+// @Component({
+//   selector: 'app-carrello',
+//   templateUrl: './carrello.component.html',
+//   styleUrls: ['./carrello.component.scss']
+// })
+// export class CarrelloComponent {
+
+
+//   @Input() pizzaSelected: any;
+//   // @Input() pizzaAdded: any;
+//   @Input() pizze: any;
+//   // @Output() pizzaAdded = new EventEmitter<any>(); //forse non dovrebbe esserci
+
+//   carrello: any[] = [];
+
+//   aggiungiPizzaAlCarrello(pizza: any) {
+//     if (this.pizzaSelected) {
+//       const pizzaNelCarrello = this.carrello.find(p => p.nome === this.pizzaSelected.nome);
+
+//       if (pizzaNelCarrello) {
+//         pizzaNelCarrello.quantita = (pizzaNelCarrello.quantita || 0) + 1;
+//       } else {
+//         this.carrello.push({ ...this.pizzaSelected, quantita: 1 });
+//       }
+//     }
+//   }
+  
+
+//   calcolaTotale(): number {
+//     let totale = 0;
+
+//       totale += this.pizzaSelected.quantita * this.pizzaSelected.prezzo;
+
+//     return totale;
+//   }
+// }
+
+
+
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
@@ -6,36 +47,32 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrls: ['./carrello.component.scss']
 })
 export class CarrelloComponent {
-
-
   @Input() pizzaSelected: any;
-  // @Input() pizzaAdded: any;
-  @Input() pizze: any;
-  // @Output() pizzaAdded = new EventEmitter<any>(); // non dovrebbe esserci
+  @Input() pizzeNelCarrello: any[] = [];
+  @Output() pizzaRimossa = new EventEmitter<any>();
+  @Output() rimuoviPizzaDalCarrello = new EventEmitter<any>();
 
   carrello: any[] = [];
 
-  aggiungiPizzaAlCarrello(pizza: any) {
-    if (this.pizzaSelected) {
-      const pizzaNelCarrello = this.carrello.find(p => p.nome === this.pizzaSelected.nome);
+  calcolaTotaleComplessivo(): number {
+    return this.pizzeNelCarrello.reduce((total, pizza) => total + pizza.quantita * pizza.prezzo, 0);
+  }
 
-      if (pizzaNelCarrello) {
-        pizzaNelCarrello.quantita = (pizzaNelCarrello.quantita || 0) + 1;
+  rimuoviPizza(pizza: any): void {
+    const pizzaNelCarrello = this.pizzeNelCarrello.find(p => p.nome === pizza.nome);
+
+    if (pizzaNelCarrello) {
+      if (pizzaNelCarrello.quantita > 1) {
+     
+        pizzaNelCarrello.quantita--;
       } else {
-        this.carrello.push({ ...this.pizzaSelected, quantita: 1 });
+       
+        const index = this.pizzeNelCarrello.indexOf(pizzaNelCarrello);
+        this.pizzeNelCarrello.splice(index, 1);
       }
+
+
+      this.rimuoviPizzaDalCarrello.emit(this.pizzeNelCarrello);
     }
   }
-  
-
-  calcolaTotale(): number {
-    let totale = 0;
-
-      totale += this.pizzaSelected.quantita * this.pizzaSelected.prezzo;
-
-    return totale;
-  }
 }
-
-
-

3b3caff63ecbf76037af45af7d9ff581e5147c86
Underscore96 <ottonelloisabella@gmail.com>
2023-12-06 15:15:24 +0100

    cc

--- src/app/pizze.service.spec.ts
+++ src/app/pizze.service.spec.ts
@@ -6,11 +6,28 @@ describe('PizzeService', () => {
   let service: PizzeService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [PizzeService],
+    });
     service = TestBed.inject(PizzeService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+
+  it('should calculate total correctly', () => {
+    // aggiungiamo le pizze da calcolare
+    service.pizzeNelCarrello = [
+      { nome: 'Margherita', quantita: 1, prezzo: 10 },
+      { nome: 'Pepperoni', quantita: 1, prezzo: 13 },
+    ];
+
+    // Calcola il totale complessivo
+    const total = service.calcolaTotaleComplessivo();
+
+    // guardiamo se il totale é corretto
+    expect(total).toBe(33); // 1 * 10 + 1 * 13 = 10 + 13 = 23
   });
 });
